86e3f904bfabee3d6bbec5341a23ce88b92bd48a
iamvickym <51700161+iamvickym@users.noreply.github.com>
2020-02-24 21:35:24 +0530

    Added Name Print Program

---
+++ NameProg.java
@@ -0,0 +1,11 @@
+public class NameProg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi ramya");
+		System.out.println("Hi Pooja");
+		System.out.println("Hi Vicky");
+		System.out.println("Hi Sam");
+	}
+
+}
